f9d34fb8003a0afad7ffdfe1284677b6aa228c0b
orodrigu <oswinrods@gmail.com>
2018-02-18 02:08:09 -0500

    add chord info, fix indexing error in writeRegisters() and moved around some code

--- led.ino
+++ led.ino
@@ -2,15 +2,14 @@ const int SER_PIN = 2;    // pin 14 on the 75HC595
 const int RCLK_PIN = 3;   // pin 12 on the 75HC595
 const int SRCLK_PIN = 4;  // pin 11 on the 75HC595
 
-const int NUM_REGS = 16;  // total registers available
-const int NUM_LEDS = 12;  // number of LEDs on fretboard
-bool led[NUM_LEDS];       // total registers that are used
-
-const bool ON = 1;        // LED state indicator
-const bool OFF = 0;       // LED state indicator
+const bool ON = 1;          // LED on-state indicator
+const bool OFF = 0;         // LED off-state indicator
+const int NUM_REGS = 16;    // total registers available
+const int NUM_LEDS = 12;    // number of LEDs on fretboard
+bool led[NUM_LEDS];         // total registers that are used
 
 /* 
- * The diagram below shows how LED
+ * The diagram below shows how led[]
  * array indices are referenced
  * ---------------------
  *      Fret Number
@@ -22,7 +21,36 @@ const bool OFF = 0;       // LED state indicator
  *|------|--03--|------||
  *|--08--|------|--00--||
  *|--09--|--04--|------||
+ *
+ * [1, 5, 9]    for G_CHORD
+ * [0, 2, 6]    for C_CHORD
+ * [3, 4, 8]    for D_CHORD
+ * [0, 3, 6, 7] for F_CHORD
+ * [0, 2, 3]    for Am_CHORD
+ * [1, 2]       for Em_CHORD
  */
+
+const int G_CHORD = 0;
+const int C_CHORD = 1;
+const int D_CHORD = 2;
+const int F_CHORD = 3;
+const int Am_CHORD = 4;
+const int Em_CHORD = 5;
+const int NUM_CHORDS = 6;
+const int MAX_LEDS_FOR_CHORD = 4;
+
+// For following, consider using a struct instead that has the
+// chord name, led indices and number of leds too
+
+const int CHORD_LED_MAPPING[NUM_CHORDS][MAX_LEDS_FOR_CHORD] = {
+  // Use -1 as flag to skip, since not a valid led[] index
+  {1, 5, 9, -1}, // G_CHORD
+  {0, 2, 6, -1}, // C_CHORD
+  {3, 4, 8, -1}, // D_CHORD
+  {0, 3, 6, 7},  // F_CHORD
+  {0, 2, 3, -1}, // Am_CHORD
+  {1, 2, -1, -1} // Em_CHORD
+};
 
 void setup(){
   // define Arduino pins
@@ -35,7 +63,7 @@ void setup(){
 }
 
 void loop(){
-}             
+}
 
 // turn led[i] OFF or ON
 void turnLed(int i, bool state){
@@ -45,8 +73,24 @@ void turnLed(int i, bool state){
 
 // turn all LEDs OFF or ON
 void turnAllLeds(bool state){
-  for(int i = NUM_LEDS; i > 0; i--){
+  setAllLeds(state);
+  writeRegisters();
+}
+
+// set, don't write, all LEDs' state
+void setAllLeds(bool state){
+  for(int i = NUM_LEDS-1; i >= 0; i--){
     led[i] = state;
+  }
+}
+
+// turn chord ON
+void turnOnChord(int chord){
+  setAllLeds(OFF);
+  for (int i = MAX_LEDS_FOR_CHORD-1; i >= 0; i--){
+    if (led[CHORD_LED_MAPPING[chord][i]] != -1) {
+      led[CHORD_LED_MAPPING[chord][i]] = 1;
+    }
   }
   writeRegisters();
 }
@@ -55,7 +99,7 @@ void turnAllLeds(bool state){
 void writeRegisters(){
   digitalWrite(RCLK_PIN, LOW);
 
-  // only using NUM_LEDS of NUM_REGS pins
+  // only using first NUM_LEDS of NUM_REGS pins
   // so populate remaining with 0 state
   for (int i = NUM_REGS - NUM_LEDS; i > 0; i--){
     digitalWrite(SRCLK_PIN, LOW);
@@ -64,7 +108,7 @@ void writeRegisters(){
   }
 
   // start backwards because of 'push' nature!
-  for(int i = NUM_LEDS; i > 0; i--){
+  for(int i = NUM_LEDS-1; i >= 0; i--){
     digitalWrite(SRCLK_PIN, LOW);
     digitalWrite(SER_PIN, led[i]);
     digitalWrite(SRCLK_PIN, HIGH);
