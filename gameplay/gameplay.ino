9cf709d7d28b40292a8c1596977bcceebca66553
orodrigu <oswinrods@gmail.com>
2018-02-24 14:22:49 -0500

    logic skeleton in comment form

--- gameplay/gameplay.ino
+++ gameplay/gameplay.ino
@@ -1,5 +1,31 @@
 void setup(){
+	// led setup
+	// screen setup
+	// fsr setup
+
+	// led initialize
+	// screen initialize
+	// fsr initialize
 }
 
 void loop(){
+	// mode prompt
+	// if chord mode, chord prompt
+	// set chord 'sequence' i.e. same chord
+	// else if key mode, key prompt
+	// set chord sequence for given key
+
+	// main logic for chord-playing:
+	// instruct how play is expected
+	// command chord to be played
+	// wait until play is complete
+	// collect sensor data about play
+	// verify play correctness
+	// give feedback about play
+	// if play is unacceptable, loop back to same chord
+	// else if play is good, next chord in sequence
+
+	// all the while:
+	// if 'change chord/key' pressed, loop back to chord/key prompt
+	// else if 'change mode' pressed, loop back to mode prompt
 }
